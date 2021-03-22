05a8ddf61653ddd3c95d63ba988197f2d254790c
krystalpc <73982964+krystalpc@users.noreply.github.com>
2021-03-23 01:02:35 +0800

    Create copeng.h

---
+++ copeng/copeng.h
@@ -0,0 +1,25 @@
+#include <string>
+
+class AirlineTicket
+{
+
+	public:
+		AirlineTicket();
+		 ~AirlineTicket();
+		
+		double calculatePriceInDollars() const;
+		
+		const  std::string& getPassengerName() const;
+		void setPassengerName(const std::string& name);
+		
+		int getNumberOfMiles() const;
+		void setNumberOfMiles(int miles);
+		
+		bool hasEliteSuperRewardsStatus() const;
+		void setHasEliteSuperRewardsStatus(bool status);
+	private:
+		std::string mPassengerName;
+		int mNumberOfMiles;
+		bool mHasEliteSuperRewardsStatus;	
+
+};
